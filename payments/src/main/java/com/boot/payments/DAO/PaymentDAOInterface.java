c05b5fbaa09f2fa75c0ab41a806a0127228437f0
kollurucharan <139054162+kollurucharan@users.noreply.github.com>
2023-09-25 00:29:38 +0530

    Delete payments directory

--- payments/src/main/java/com/boot/payments/DAO/PaymentDAOInterface.java
+++
@@ -1,9 +0,0 @@
-package com.boot.payments.DAO;
-
-import java.util.ArrayList;
-
-import com.boot.payments.model.PaymentModel;
-
-public interface PaymentDAOInterface {
-	public ArrayList<PaymentModel> getAllTransaction();
-}
--- payments/src/main/java/com/boot/payments/repository/PaymentRepositoryInterface.java
+++
@@ -1,9 +0,0 @@
-package com.boot.payments.repository;
-
-import java.util.ArrayList;
-
-import com.boot.payments.model.PaymentModel;
-
-public interface PaymentRepositoryInterface {
-	public ArrayList<PaymentModel> getAllTransaction();
-}
